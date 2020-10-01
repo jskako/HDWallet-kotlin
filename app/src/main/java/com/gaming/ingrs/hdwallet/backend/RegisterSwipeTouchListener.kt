d6b6e5f0ba7db38a30c3e13067e4da34d06154c7
jskako <jskako@gmail.com>
2020-10-02 01:21:34 +0200

    Added QR icon

--- app/src/main/java/com/gaming/ingrs/hdwallet/backend/RegisterSwipeTouchListener.kt
+++ app/src/main/java/com/gaming/ingrs/hdwallet/backend/RegisterSwipeTouchListener.kt
@@ -1,17 +1,11 @@
 package com.gaming.ingrs.hdwallet.backend
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
-import com.gaming.ingrs.hdwallet.MainActivity
-import com.gaming.ingrs.hdwallet.R
 
 
 class RegisterSwipeTouchListener: AppCompatActivity() {
